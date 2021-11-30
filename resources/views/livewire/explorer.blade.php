13838d95be1aaecfce3c2769188e1e7e3532ec6e
Efrain Salas <efrainsalas@icloud.com>
2021-11-30 23:26:08 +0100

    Trying to fix wrong folder upload error

--- resources/views/livewire/explorer.blade.php
+++ resources/views/livewire/explorer.blade.php
@@ -206,7 +206,7 @@
 
                                 <div class="ml-auto inline-flex gap-2 items-center">
                                     @if ($folder && ($folder->canWrite() || $folder->canUpload()))
-                                        <x-j-button id="bt-upload-files">
+                                        <x-j-button id="bt-upload-files-{{ $folderId }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="inline h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                                             </svg>
@@ -225,7 +225,7 @@
                                                     folderId: '{{ $folderId }}',
                                                 },
                                             });
-                                            resumable.assignBrowse(document.getElementById('bt-upload-files'));
+                                            resumable.assignBrowse(document.getElementById('bt-upload-files-{{ $folderId }}'));
                                             resumable.on('fileAdded', function(file, event){
                                                 @this.addToQueue(file.fileName);
                                                 resumable.upload();
