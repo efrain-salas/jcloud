533824c52622d948966e09079c8b2856a271ce86
Efrain Salas <efrainsalas@icloud.com>
2021-11-30 14:26:05 +0100

    Remove unused code

--- resources/views/livewire/explorer.blade.php
+++ resources/views/livewire/explorer.blade.php
@@ -245,7 +245,7 @@
                                     <!-- <input wire:model="uploadedFiles" type="file" multiple class="absolute inset-0 opacity-0" /> -->
 
                                     @if (($folder && $folder->canWrite()) || ! $folder)
-                                        <x-j-button wire:click="openNewFolderModal" wire:key="new-folder-button" onclick="console.log('hola?')">
+                                        <x-j-button wire:click="openNewFolderModal" wire:key="new-folder-button">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="inline h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
                                             </svg>
